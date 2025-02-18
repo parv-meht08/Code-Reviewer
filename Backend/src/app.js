6c5eec32da918c448820daa579c25e0e0b1f4f7b
Parv Mehta <mehtaparv21@gmail.com>
2025-02-18 19:46:58 +0530

    Frontend Done and integreate with backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require ('express')
 const aiRoute = require('./routes/ai.route')
+const cors = require('cors')
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/ai', aiRoute);
 
--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -1,12 +1,12 @@
 const aiService = require("../services/ai.service");
 
-module.exports.getResponse = async (req, res) => {
-  const prompt = req.query.prompt;
+module.exports.getReview = async (req, res) => {
+  const code = req.body.code;
 
-  if (!prompt) {
-    return res.status(400).send("Please provide a prompt");
+  if (!code) {
+    return res.status(400).send("Please provide a code");
   }
 
-  const result = await aiService.generateContent(prompt);
+  const result = await aiService.generateContent(code);
   res.send(result);
 };
